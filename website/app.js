7ed8a966f7f281dd5f3aa13ebdea3e4afcb2c0c0
Riste <adziev@gmail.com>
2017-08-31 01:13:27 +0300

    smena na pi Pro/sam ap endpoint

--- website/app.js
+++ website/app.js
@@ -12,9 +12,9 @@ var game = new Phaser.Game(width, height, Phaser.AUTO, 'aws-serverless-samfarm',
 var sprites;
 var DESIRED_SAM_COUNT = 1;
 var CURRENT_SAM_COUNT = 1;
-var MAX_SAM_CHANGE = 10;
+var MAX_SAM_CHANGE = 10 ;
 var LAST_CHANGE_TIME = Date.now();
-var GET_SAM_COUNT_URL =  'https://1yhfbnimr1.execute-api.us-east-1.amazonaws.com/Prod/sam';
+var GET_SAM_COUNT_URL =  'https://1yhfbnimr1.execute-api.us-east-1.amazonaws.com/Stage/sam';
 var TIME_BETWEEN_COUNT_UPDATE_MS = 4000;
 
 function preload() {
